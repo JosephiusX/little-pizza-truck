121b5bb0cd628a1f3d4a1bac328040e75306c3d2
josephius x <josephiusx@gmail.com>
2023-05-10 09:22:45 -0700

    changed main.jsx to index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   ],
   build: {
     rollupOptions: {
-      input: './src/index.html',
+      input: 'server\client\public\index.html',
     },
     sourcemap: true,
   },
