d2577b79c115cb3f396df4b18094737be0c2a975
josephius x <josephiusx@gmail.com>
2023-05-10 08:59:35 -0700

    ?

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
+import { splitVendorChunkPlugin } from 'vite';
 import path from 'path';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+// import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [react(), WindiCSS(), reactRefresh()],
+  entry: './src/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    react(),
+    WindiCSS(),
+    splitVendorChunkPlugin(),
+    // reactRefresh()
+  ],
   build: {
     rollupOptions: {
-      input: 'C:/Users/josep/OneDrive/Desktop/little-pizza-truck/server/client/public/index.html',
+      input: './src/main.jsx',
     },
     sourcemap: true,
   },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src'),
-    },
+  alias: {
+    '~': path.resolve(__dirname, 'src')
   },
   optimizeDeps: {
-    include: ['lodash'],
+    include: ['lodash']
   },
   server: {
     port: 3000,
@@ -34,12 +43,12 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        indentedSyntax: true,
-      },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          indentedSyntax: true
+        }
+      }
     },
   },
 });
